f4d9c9807b44a0499ed3083c8c81250408f798d8
jmunozz <j1805@hotmail.com>
2024-02-06 14:38:09 +0100

    FIX: image + favicon

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = {
       "user-images.githubusercontent.com",
       "res.cloudinary.com",
 
+      // Image from github pages
+      "alexandrieapp.github.io",
+
       // Images from Notion
       "s3.us-west-2.amazonaws.com",
       "prod-files-secure.s3.us-west-2.amazonaws.com",
